b049e3391d878961eb18e6a2a46d9ee60d183778
Ben Word <ben@benword.com>
2026-03-19 11:07:18 -0500

    Rename import alias wpcomposergo → wppackagesgo in main.go and migrate_test.go

--- cmd/wppackages/main.go
+++ cmd/wppackages/main.go
@@ -4,12 +4,12 @@ import (
 	"fmt"
 	"os"
 
-	wpcomposergo "github.com/roots/wp-packages"
+	wppackagesgo "github.com/roots/wp-packages"
 	"github.com/roots/wp-packages/cmd/wpcomposer/cmd"
 )
 
 func main() {
-	cmd.Migrations = wpcomposergo.Migrations
+	cmd.Migrations = wppackagesgo.Migrations
 	if err := cmd.Execute(); err != nil {
 		fmt.Fprintf(os.Stderr, "%v\n", err)
 		os.Exit(1)
